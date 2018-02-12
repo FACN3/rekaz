dff31d77b0f5a5ad6bdc7913ad0742cc27daee0a
hasansaad1 <hasansaad.saad1998@gmail.com>
2018-02-12 14:35:57 +0200

    created the button component reference #20

--- src/components/Landing_page.js
+++ src/components/Landing_page.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Abutton from './Main_button';
 const LandingPage = () => {
   return (<div className="container">
     <div className="header">
@@ -8,6 +8,7 @@ const LandingPage = () => {
     <div className="div-introduction">
       <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</h2>
     </div>
+    <Abutton title="Smoking" img="../../public/assets/smoke-512.png"/>
   </div>);
 }
 
