c32f1887d879d8c9667aa4a372527adf22f73c1c
prodionov <pavelrodionov@hotmail.com>
2018-02-18 16:50:23 +0200

    added counters to the first chart Reference #31

---
+++ src/components/First_chart_counter.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import Circle from "../model/d3_counter";
+
+class Counter extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { counter: 0, value: 60 };
+    this.startCounter = this.startCounter.bind(this);
+    this.counterCirclesArray = [
+      new Circle(`counter${this.props.position}`, 50, 50, 50, 0)
+    ];
+  }
+
+  componentDidMount() {
+    const width = window.outerWidth;
+    const svg = d3.select(".counter" + this.props.position);
+    const svgWidth = width * 0.25;
+    const svgHeight = width * 0.25;
+    svg.attr("width", svgWidth).attr("height", svgHeight);
+    let radius = width / 10;
+    svg
+      .selectAll("g")
+      .data(this.counterCirclesArray)
+      .enter()
+      .append("g")
+      .attr("transform", function(d) {
+        return `translate(${radius}, ${radius})`;
+      })
+      .append("circle")
+      .attr("r", radius)
+      .attr("cx", 0)
+      .attr("cy", 0)
+      .attr("fill", "lightgreen");
+
+    svg
+      .selectAll("g")
+      .append("text")
+      .attr("id", `counter${this.props.position}`)
+      .attr("fill", "black")
+      .style("font-size", `${radius / 2}px`)
+      .style("font-family", "Robot")
+      .style("font-weight", "bold")
+      .attr("anchor", "middle")
+      .attr("x", -0.65 * radius)
+      .text(function(d) {
+        return "0%";
+      });
+
+    this.startCounter();
+  }
+
+  componentDidUpdate() {
+    this.startCounter();
+  }
+
+  startCounter() {
+    const svg = d3.select(".counter" + this.props.position);
+    {
+      this.counterCirclesArray.forEach((value, i) => {
+        value.updateState(this.props.value);
+      });
+
+      svg
+        .select(`#counter${this.props.position}`)
+        .transition()
+        .duration(2000)
+        .ease(d3.easeSin)
+        .tween("text", function(d) {
+          let that = d3.select(this),
+            i = d3.interpolate(d.previousState / 100, d.currentState / 100);
+          return function(t) {
+            that.text(d3.format(".1%")(i(t)));
+          };
+        });
+    }
+  }
+
+  render() {
+    return <svg className={"counter" + this.props.position} />;
+  }
+}
+export default Counter;
