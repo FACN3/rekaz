d41ab17d4dd0c58889168c82b55d6016a9d25ec7
Ghassan <ghassan.maslamani@gmail.com>
2018-02-21 16:07:10 +0200

    Adding fb sharing button Relates #58

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,6 +8,11 @@ const Navbar = (props) => {
       <option value="ar">العربية
       </option>
     </select>
+    <div className="fb-share-button"
+  data-href="https://www.your-domain.com/your-page.html"
+  data-layout="button_count">
+</div>
+
   </div>);
 }
 
